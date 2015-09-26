c63a032f17db3d2b84a63ce5fff0f6740973eedc
BigBot96 <adamlgarrett89@gmail.com>
2015-09-26 13:45:14 -0400

    mkvendor.sh: Initial commit of gts210wifi

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := gts210wifi
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/gts210wifi/device_gts210wifi.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gts210wifi
+PRODUCT_NAME := cm_gts210wifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := gts210wifi
+PRODUCT_MANUFACTURER := samsung
